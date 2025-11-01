35b5315884cfd8929dcf9a514822b7b8b6b27d26
connorjames676 <connorjames676@gmail.com>
2025-11-01 16:13:14 +0000

    Created Profile Model

---
+++ database/migrations/2025_11_01_160939_create_profiles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            // Setup up a one-to-one relationship with a user
+            $table->string('bio');
+            $table->integer('number_of_badges');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
