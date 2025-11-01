b725844ac264ace81881de2fbe49551033a98649
connorjames676 <connorjames676@gmail.com>
2025-11-01 15:47:30 +0000

    Created Badge Model

---
+++ database/migrations/2025_11_01_154402_create_badges_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('badges', function (Blueprint $table) {
+            $table->id();
+            // Set up participant ID of person that achieved badge
+            // Set up attempt ID of the attempt that earned the badge
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('badges');
+    }
+};
