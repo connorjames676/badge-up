8afe174fcbadeb08e7a7623ac903367d33cf449d
connorjames676 <connorjames676@gmail.com>
2025-11-01 15:43:31 +0000

    Created Comment Model

---
+++ database/migrations/2025_11_01_154050_create_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            // Set up participant ID of user that has comment
+            // Set up attempt ID of attempt commented on
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
