0ff3f93046ca8193d7d4801087af2164dfa6e389
connorjames676 <connorjames676@gmail.com>
2025-11-01 16:31:01 +0000

    Created MyUser Model and deleted Coach and Participant

--- database/migrations/2025_11_01_162749_create_my_users_table.php
+++ database/migrations/2025_11_01_162749_create_my_users_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('participants', function (Blueprint $table) {
+        Schema::create('my_users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
             $table->string('password');
             $table->integer('age');
+            $table->enum('role', ['coach','participant']);
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('participants');
+        Schema::dropIfExists('my_users');
     }
 };
