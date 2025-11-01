6e3a6d0e541fc0d5efccdb13aac7882fddc871dd
connorjames676 <connorjames676@gmail.com>
2025-11-01 15:24:10 +0000

    Created Participant Model

--- database/migrations/2025_11_01_152202_create_participants_table.php
+++ database/migrations/2025_11_01_152202_create_participants_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('coaches', function (Blueprint $table) {
+        Schema::create('participants', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('coaches');
+        Schema::dropIfExists('participants');
     }
 };
