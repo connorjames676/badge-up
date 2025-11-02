fec7e1d1d88dcc8f429b974ff77d1f326e6e6300
connorjames676 <connorjames676@gmail.com>
2025-11-02 13:31:18 +0000

    Fixed issue with seeding for unique profiles

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -21,8 +21,8 @@ class ProfileFactory extends Factory
             // Generates random text (seems the best way to replicate a bio)
             'bio' => fake()->realText(100, 3),
             'number_of_badges' => fake()->randomFloat(0, 0, 50),
-            // Generates a my_user_id from the next available user in the my_user table
-            'my_user_id' => MyUser::inRandomOrder()->first()->id, 
+            // Generates a unique my_user_id 
+            'my_user_id' => fake()->unique()->numberBetween(2, 51), // Because Connor is id = 1
         ];
     }
 }
--- database/migrations/2025_11_01_160939_create_profiles_table.php
+++ database/migrations/2025_11_01_160939_create_profiles_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
 
             $table->bigInteger('my_user_id')->unsigned();
 
-            $table->foreign('my_user_id')->references('id')->on('my_users')->onDelete('cascade')
-                ->onUpdate('cascade');
+            $table->foreign('my_user_id')->references('id')->on('my_users')
+                ->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
