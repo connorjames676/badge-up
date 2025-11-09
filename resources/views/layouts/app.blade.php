303dc4be6905bad52ef0724bffce4ab50c3fe1bc
connorjames676 <connorjames676@gmail.com>
2025-11-09 23:24:41 +0000

    Added app.blade.php for master layout and MyUserController.php to start making actions

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Challenge Application - @yield('title')</title>
+	</head>
+	<body>
+		<h1>Challenge Appp - @yield('title')</h1>
+		
+		<div>
+			@yield('content')
+		</div>
+	</body>
+</html>
